0609e374c710a734de3e4a8ffca0c0aa13d431ff
mohdzaid <mohdzaid@gmail.com>
2023-02-02 12:04:29 +0530

    added refactor and update code

--- src/modules/codesnippets/typeDef/resolver-type.ts
+++ src/modules/codesnippets/typeDef/resolver-type.ts
@@ -18,3 +18,26 @@ export class SnippetDef {
   @IsString()
   readonly description: string;
 }
+
+@ObjectType()
+export class GetSnippetByIdDef {
+  @Field({ description: 'id of the snippet', nullable: false })
+  @IsNotEmpty()
+  @IsString()
+  readonly _id: string;
+
+  @Field({ description: 'title of the snippet', nullable: false })
+  @IsNotEmpty()
+  @IsString()
+  readonly title: string;
+
+  @Field({ description: 'description of the snippet', nullable: false })
+  @IsNotEmpty()
+  @IsString()
+  readonly description: string;
+
+  @Field({ description: 'description of the snippet', nullable: false })
+  @IsNotEmpty()
+  @IsString()
+  readonly codeSnippets: string;
+}
